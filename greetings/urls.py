c6e4003388977f1dbf780e4c3b74268967c3e2d8
d.baldassarra <d.baldassarra@reply.it>
2020-11-06 16:10:51 +0100

    http://localhost:8000/greetings/insert?name=nome&surname=cognome

--- greetings/urls.py
+++ greetings/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^getList/', views.greetingsList.as_view())
+
+    url(r'^getList/', views.greetingsList.as_view()),
+    #http://localhost:8000/greetings/getList/
+
+    url(r'^insert/', views.insert.as_view())
+    #http://localhost:8000/greetings/insert?name=nome&surname=cognome
 ]
--- greetings/views.py
+++ greetings/views.py
@@ -6,6 +6,13 @@ from rest_framework.views import APIView
 
 from . serializers import GreetingSerializer
 from . models import Greeting
+
+from django.utils.timezone import now
+from django.db import models
+from django.db import connection
+
+from django.utils import timezone
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the greetings index 4.")
@@ -17,3 +24,19 @@ class greetingsList(APIView):
         #return HttpResponse(greetings1[0].name)
         serializer = GreetingSerializer(greetings1, many=True)
         return Response(serializer.data)
+
+class insert(APIView):
+    def __maxId(self):
+        cursor = connection.cursor()
+        cursor.execute('SELECT max(id)+1 as max_id FROM greetings_greeting')
+        return cursor.fetchone()[0]
+
+    def get(self, request):
+        sysdate = timezone.now()
+        sysdateString = str(sysdate.day) + "_" + str(sysdate.month) + "_" + str(sysdate.year)
+        greeting = Greeting()
+        greeting.id = self.__maxId()
+        greeting.name = request.GET.get('name', "name" + sysdateString)
+        greeting.surname = request.GET.get('surname', "surname" + sysdateString)
+        greeting.save()
+        return HttpResponse(greeting.name + " " + greeting.surname)
